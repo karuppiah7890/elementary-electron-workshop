c975223e404be384ceedb8234d027851c6ad1a40
Karuppiah <karuppiah7890@gmail.com>
2016-05-20 00:19:03 +0530

    Add save pdf module

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 var picture = require('cat-picture');
 var image = require('lightning-image-poly');
+var fs = require('fs');
+var remote = require('electron').remote;
 
 var src = picture.src;
 picture.remove();
 
 var viz = new image('#visualization',null,[src],{hullAlgorithm: 'convex'});
+
+function save() {
+  remote.getCurrentWindow().webContents.printToPDF({portrait:true},function(err,data){
+      if(err)
+        return alert('Error generating PDF! ' + error.message);
+
+      fs.writeFile('annotation.pdf',data,function(err){
+        if(err)
+        return alert('Error generating PDF! ' + error.message);
+
+        alert('PDF saved!');
+      });
+  });
+}
+
+window.addEventListener('keydown',function(e){
+  if(e.keyCode==80)
+  save();
+});
